50456ffe4b4b66818a4a4f473a8561d8052ee3f4
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2022-10-04 11:36:14 +0200

    Initial commit

---
+++ Rennschnecken/Rennschnecken/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rennschnecken
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            const int STRECKEN_LÄNGE = 50;
+
+            Random random = new Random();
+
+            Rennschnecke schnecke1 = new Rennschnecke(random);
+            Rennschnecke schnecke2 = new Rennschnecke(random);
+            Rennschnecke schnecke3 = new Rennschnecke(random);
+
+            do
+            {
+                schnecke1.Krieche();
+                schnecke2.Krieche();
+                schnecke3.Krieche();
+
+                Console.Clear();
+                Console.WriteLine(schnecke1.Visualisierung);
+                Console.WriteLine(schnecke2.Visualisierung);
+                Console.WriteLine(schnecke3.Visualisierung);
+
+                Thread.Sleep(250);
+            }
+            while ((schnecke1.Strecke < STRECKEN_LÄNGE) && (schnecke2.Strecke < STRECKEN_LÄNGE) && (schnecke3.Strecke < STRECKEN_LÄNGE));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rennschnecken/Rennschnecken/Rennschnecken.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rennschnecken
+{
+    internal class Rennschnecken
+    {
+        public void Rennschnecke(Random rand)
+        {
+            _rand = rand;
+        }
+
+        public void Krieche()
+        {
+            _strecke += _rand.Next(4);
+        }
+
+        public int Strecke
+        {
+            get
+            {
+                return _strecke;
+            }
+        }
+
+        public string Visualisierung
+        {
+            get
+            {
+                string s = "";
+
+                for (int i = 0; i < _strecke; i++)
+                {
+                    s += "_";
+                }
+                s += "@";
+
+                return s;
+            }
+        }
+
+        private int _strecke = 0;
+        private Random _rand;
+
+    }
+}
