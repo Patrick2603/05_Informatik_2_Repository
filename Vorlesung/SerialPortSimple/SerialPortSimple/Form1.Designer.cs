5f6ef93f2aa99d3549fd10385f3dc0df8b790467
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2022-10-05 13:22:42 +0200

    serial port simple import

---
+++ Vorlesung/SerialPortSimple/SerialPortSimple/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace SerialPortSimple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 11);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 186);
+            this.textBox1.TabIndex = 0;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(219, 10);
+            this.StartButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(56, 34);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "los";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 206);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Serial Port Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button StartButton;
+    }
+}
+
---
+++ Vorlesung/SerialPortSimple/SerialPortSimple/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialPortSimple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            SerialPort serialPort = new SerialPort();
+
+            serialPort.PortName = "COM4";
+            serialPort.BaudRate = 9600;
+            serialPort.DtrEnable = true;
+
+            serialPort.Open();
+            Thread.Sleep(3000);
+
+            byte[] command = new byte[] { 0x7F };
+            serialPort.Write(command, 0, 1);
+            string deviceName = serialPort.ReadLine();
+
+            byte[] command = new byte[] { 0x7E };
+            serialPort.Write(command, 0, 1);
+            string serialNumber = serialPort.ReadLine(); 
+
+            serialPort.Close();
+
+            textBox1.Text = deviceName;
+            textBox1.Text = serialNumber; 
+        }
+    }
+}
