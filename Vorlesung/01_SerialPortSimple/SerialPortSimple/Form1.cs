62c475393afa5c0f35e8844dd52ac917d8564cf6
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2022-10-10 13:09:42 +0200

    name and number working + ioexception

--- Vorlesung/01_SerialPortSimple/SerialPortSimple/Form1.cs
+++ Vorlesung/01_SerialPortSimple/SerialPortSimple/Form1.cs
@@ -24,42 +24,39 @@ namespace SerialPortSimple
         {
             SerialPort serialPort = new SerialPort();
 
-            serialPort.PortName = "COM4";
+            serialPort.PortName = "COM5";
             serialPort.BaudRate = 9600;
             /* Data Terminal Ready */ 
             serialPort.DtrEnable = true;
 
-            serialPort.Open();
-            Thread.Sleep(3000);
-
-            /* is getting executed normally if error witch is handled in catch function it jumps out */ 
-            try  
+            /* is getting executed normally if error witch is handled in catch function it jumps out */
+            try
             {
+                serialPort.Open();
+                Thread.Sleep(3000);
                 byte[] command = new byte[] { 0x7F };
                 serialPort.Write(command, 0, 1);
                 string deviceName = serialPort.ReadLine();
 
-                byte[] command1 = new byte[] { 0x7E };
-                serialPort.Write(command1, 0, 1);
+                byte[]  Number = new byte[] { 126 };
+                serialPort.Write(Number, 0, 1);
                 string serialNumber = serialPort.ReadLine();
 
-                textBox1.Text = "Gerät: " + deviceName;
-                textBox1.Text = "\r\n";
-                textBox1.Text = "S/N: " + serialNumber;
-            }
-
-            /* catch errors and handle it */ 
-            catch (IOException ex)  
-            {
-                textBox1.Text = "Fehler beim lesen/schreiben";
-                textBox1.Text = ex.Message;
-            }
-
-            /* executed in every case, needs to be spesifically closed */
-            finally 
-            {
+                textBox1.Text = "Gerät: " + deviceName + "\n" + "S/N: " + serialNumber;
                 serialPort.Close();
             }
+
+            /* catch errors */ 
+            catch (IOException ex)  
+            { 
+                textBox1.Text = ex.Message; 
+            }
+
+            /* executed in every case, needs to be specifically closed */
+            //finally 
+            //{
+            //    serialPort.Close();
+            //}
         }
     }
 }
