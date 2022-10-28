cc787102fa4d99ccc318a0c11e06697694211c3b
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2022-10-28 06:48:12 +0200

    bissle kommentiert

--- Vorlesung/03_EventDemo/EventDemo/Form1.cs
+++ Vorlesung/03_EventDemo/EventDemo/Form1.cs
@@ -17,14 +17,17 @@ namespace EventDemo
             InitializeComponent();
             _widerstand = new Widerstand("Pull-Up");
             currDesc.Text = _widerstand.Description;
+            /* Schritt 5: Anmelden des Handlers am Event als Beobachter */
             _widerstand.DescriptionChanged += DescChanged;
         }
-
+        /* Schritt 4: Event Handler schreiben. Event Handler ist eine Methode mit einer Signatur die der Delegattyp fordert 
+            mehr oder weniger der Briefkasten, Objekt muss auch vom Typ Widerstand sein dsa es so auch angelegt wurde */ 
         private void DescChanged(Widerstand widerstand)
         {
+            /* Code der ausgeführt wird wenn Event eintritt */
             currDesc.Text = _widerstand.Description;
         }
-
+        /* Event Handler */
         private void button1_Click(object sender, EventArgs e)
         {
             _widerstand.Description = newDesc.Text;
--- Vorlesung/03_EventDemo/EventDemo/Widerstand.cs
+++ Vorlesung/03_EventDemo/EventDemo/Widerstand.cs
@@ -8,9 +8,13 @@ namespace EventDemo
 {
     class Widerstand
     {
+        private string _desc;
+        /* Schritt 1: Delegatendtyp für den Evenet Handler deklarieren, bei Übertragung der Events wird immer das Objekt mitgegeeben, wie ID zu verstehen */
         public delegate void DescriptionChangedHandler(Widerstand w);
+        /* Schritt 2: Event in der auszulösenden Klasse definieren, Name des Events DescriptionChanged */ 
         public event DescriptionChangedHandler DescriptionChanged;
 
+        /* Konstrukor */ 
         public Widerstand(string desc)
         {
             _desc = desc;
@@ -18,15 +22,21 @@ namespace EventDemo
 
         public string Description
         {
-            get { return _desc; }
+            get 
+            { 
+                return _desc; 
+            }
 
             set
             {
                 _desc = value;
+                /* 3.Schritt: Event wird ausgelöst */
                 OnDescriptionChanged();
             }
         }
-
+        /* Schritt 3: Es wird immer am Afang geprüft ob dsa Event null ist, Grund dafür kein Handler am Event angemeldet, 
+            Form1 möchte wissen welches Widerstand Objekt hat es ausgelöst bekommt deshalb Referenz des Objekts -> this Referenz  
+            Form1 kann nur drauf zu greifen wenn es die ID des Objekts hat */ 
         private void OnDescriptionChanged()
         {
             if (DescriptionChanged != null)
@@ -34,7 +44,5 @@ namespace EventDemo
                 DescriptionChanged(this);
             }
         }
-
-        private string _desc;
     }
 }
