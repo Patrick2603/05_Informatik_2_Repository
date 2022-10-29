37ea5869445fd7315a273f72b487b10052a1f916
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2022-10-30 01:24:09 +0200

    Zeitgeber finished

---
+++ Übungsaufgaben/Zeitgeber/Zeitgeber/Zeitgeber/Zeitgeber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading; 
+
+namespace Zeitgeber 
+{
+    internal class Zeitgeber
+    {
+        /* Schritt 1 */
+        public delegate void TickChangedHandler(Zeitgeber source);
+        /* Schritt 2 */ 
+        public event TickChangedHandler TickChanged;
+
+        /* Schritt 3 */
+        public void Start()
+        {
+            Thread.Sleep(1000);
+
+            if(TickChanged != null)
+            {
+                TickChanged(this);
+            }
+        }
+    }
+}
