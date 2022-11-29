c2824db8f234a7f4b87a3df4a3927cc6cc711554
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2022-11-29 16:25:02 +0100

    add class Zählerstandverwaltung

---
+++ Labor-5_Collections/SerielleKommunikation/Zählerstandverwaltung.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerielleKommunikation
+{
+    class Zählerstandverwaltung
+    {
+        private List<int> _zählerstandVerlauf; 
+
+        public Zählerstandverwaltung()
+        {
+            
+        }
+        public List<int> Zählerstand
+        {
+            get
+            {
+                return _zählerstandVerlauf; 
+            }
+        }
+    }
+}
