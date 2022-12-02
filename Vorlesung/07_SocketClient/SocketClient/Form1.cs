82976622213c9cab947beca7357e64456628b354
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2022-12-02 15:32:28 +0100

    add comments in tcp project

--- Vorlesung/07_SocketClient/SocketClient/Form1.cs
+++ Vorlesung/07_SocketClient/SocketClient/Form1.cs
@@ -21,18 +21,24 @@ namespace SocketClient
         private void button1_Click(object sender, EventArgs e)
         {
             textBox1.Text = "Verbinde mich mit Server..." + Environment.NewLine;
+            /* client to server, ip adress 127.0.0.1 at port 8080 */
+            /* with new already a connection is done */
             TcpClient client = new TcpClient("localhost", 8080);
             textBox1.Text += "Verbindung aufgebaut." + Environment.NewLine;
 
             string nachricht = "Palimpalam";
             // Nachricht in Byte umwandeln
+            /* encode string to byte that it can be send */
             Byte[] daten = Encoding.ASCII.GetBytes(nachricht);
 
+            /* referenz on networkStream, get stream object of the socket to later read the data */
             NetworkStream stream = client.GetStream();
 
+            /* write the data on the stream with .write exactly like .read */
             textBox1.Text += "Sende Nachricht an Server" + Environment.NewLine;
             stream.Write(daten, 0, daten.Length);
 
+            /* close open connections */
             stream.Dispose();
             client.Close();
         }
--- Vorlesung/07_SocketServer/SocketServer/Program.cs
+++ Vorlesung/07_SocketServer/SocketServer/Program.cs
@@ -12,25 +12,36 @@ namespace SocketServer
     {
         public static void Main()
         {
+            /* ip adress at whitch the server waits */ 
+            /* 127.0.0.1 can also be expressed as "localhost" */
             IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            /* new socket is generated from OS */ 
+            /* server waits at port 8080 for any client requests */
             TcpListener serverSocket = new TcpListener(localAddr, 8080);
 
             // Das Warten auf Anfragen starten
+            /* server starts to wait for request */
             serverSocket.Start();
 
             while (true)
             {
                 Console.WriteLine("Warte auf Verbindung.... ");
                 // Blockierendes Warten, bis eine Verbindungsanfrage kommt
+                /* program stops and just continues when connection is ok */ 
+                /* if connected: client gets a new port from server so the localhost isnt blocked */
                 TcpClient client = serverSocket.AcceptTcpClient();
                 Console.WriteLine("Verbindung hergestellt");
 
                 // Das Stream-Objekt des Sockets holen
+                /* get stream object of the socket to later read the data */ 
                 NetworkStream stream = client.GetStream();
 
                 // Eingehende Daten aus dem Stream lesen
                 int i;
+                /* buffer for .read or .write, needs to be done with a buffer because of byte communication */
+                /* stream.Read(buffer, offset, size) */
                 Byte[] bytes = new Byte[256];
+                /* server is running all the time until data comes in, then it gets decoded to string and printed in the console */ 
                 while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
                 {
                     // Gelesene Bytes in einen String umwandeln
