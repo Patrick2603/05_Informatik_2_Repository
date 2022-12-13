e6d047787d4e040dc31b66d504925ceb0b8cdc57
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2022-12-13 17:28:03 +0100

    SS22 Aufgabe 3 done

--- Prüfungen/SS_22/Aufgabe_3/Aufgabe_3/Form1.Designer.cs
+++ Prüfungen/SS_22/Aufgabe_3/Aufgabe_3/Form1.Designer.cs
@@ -29,13 +29,46 @@ namespace Aufgabe_3
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ausgabe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Klausur";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Prüfungen/SS_22/Aufgabe_3/Aufgabe_3/Form1.cs
+++ Prüfungen/SS_22/Aufgabe_3/Aufgabe_3/Form1.cs
@@ -16,5 +16,21 @@ namespace Aufgabe_3
         {
             InitializeComponent();
         }
+
+        /* Teilaufgabe e */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            /* MessageBox.Show(string text, string caption) */
+            MessageBox.Show(text, "Überschrift");
+        }
+
+        /* Teilaufgabe f */
+        public void SetzeTextAusAnderemThread(string zuSchreibenderText)
+        {
+            /* Funktion wird aus anderem Thread aufgerufen */
+            /* String zuSchreibenderText wird von einem anderen Thread threadsicher in die textBox1 gesetzt */
+            textBox1.Invoke((MethodInvoker)(() => { textBox1.Text = zuSchreibenderText; })); 
+        }
     }
 }
