998d6a6f8fe3a93f6481a29afafce7891ab17902
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2022-12-14 18:02:36 +0100

    SS22 Aufg 4 done

--- Prüfungen/SS_22/Aufgabe_4/Aufgabe_4/Wohnung.cs
+++ Prüfungen/SS_22/Aufgabe_4/Aufgabe_4/Wohnung.cs
@@ -6,7 +6,45 @@ namespace Aufgabe_4
 {
     class Wohnung
     {
-        private double _miete; 
+        private double _miete;
+        /* a.) */
+        private Person _vermieter; 
+        private List<Person> _mieter;
+        
+        public Wohnung(double miete, Person vermieter)
+        {
+            _vermieter = vermieter;
+            _miete = miete;
+            /* b.) */
+            _mieter = new List<Person>(); 
+        }
 
+        public double Miete
+        {
+            get { return _miete; }
+        }
+
+        /* c.) */
+        public List<Person> Mieter
+        {
+            get { return _mieter; }
+        }
+
+        /* d.) */
+        public int MieterHinzufügen(Person p)
+        {
+            /* check if Anzahl Mieter >= 5 && Person schon in Liste enthalten */
+            if(_mieter.Count < 5 && _mieter.Contains(p) == false)
+            {
+                /* add Person to List */
+                _mieter.Add(p);
+                /* return number of Mieter */
+                return _mieter.Count;
+            }
+            else
+            {
+                return -1; 
+            }
+        }
     }
 }
