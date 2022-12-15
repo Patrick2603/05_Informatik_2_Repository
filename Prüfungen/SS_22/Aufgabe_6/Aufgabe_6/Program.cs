17bf75bf6c3e57d5d642135b6ea961b62db49188
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2022-12-15 14:48:57 +0100

    SS22 Aufg 6 done

--- Prüfungen/SS_22/Aufgabe_6/Aufgabe_6/Program.cs
+++ Prüfungen/SS_22/Aufgabe_6/Aufgabe_6/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO; 
 
 namespace Aufgabe_6
 {
@@ -6,7 +7,27 @@ namespace Aufgabe_6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                /* Create an instance of StreamReader to raed from the file */
+                /* The using statement also closes the StreamReader */
+                using (StreamReader reader = new StreamReader("faust.txt"))
+                {
+                    string line; 
+                    /* Read and display lines from the file until the end of the file is reached */
+                    while((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+
+            /* catch Exceptions and print them in the Console */
+            catch(Exception e)
+            {
+                Console.WriteLine("File could not be read");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
