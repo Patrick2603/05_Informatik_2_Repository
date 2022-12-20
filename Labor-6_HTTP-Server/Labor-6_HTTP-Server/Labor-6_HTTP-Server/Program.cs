00f823e67519b1019cf276b578f1a410ed4decee
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2022-12-20 11:09:43 +0100

    create Labor 6

---
+++ Labor-6_HTTP-Server/Labor-6_HTTP-Server/Labor-6_HTTP-Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Labor_6_HTTP_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
