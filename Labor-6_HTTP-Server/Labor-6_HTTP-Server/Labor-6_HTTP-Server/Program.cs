b9b3eaf8b1a6b68fe01df9c841a4a5e5d9eb2273
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2022-12-20 11:25:56 +0100

    server Socket + Comm over Thread

--- Labor-6_HTTP-Server/Labor-6_HTTP-Server/Labor-6_HTTP-Server/Program.cs
+++ Labor-6_HTTP-Server/Labor-6_HTTP-Server/Labor-6_HTTP-Server/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading; 
 
 namespace Labor_6_HTTP_Server
 {
@@ -6,7 +9,31 @@ namespace Labor_6_HTTP_Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*  Server Socket */
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            TcpListener serverSocket = new TcpListener(localAddr, 8080);
+            serverSocket.Start(); 
+
+            while(true)
+            {
+                TcpClient client = serverSocket.AcceptTcpClient();
+                /* Communication Thread */
+                Thread clientThread = new Thread(ClientHandler);
+                clientThread.Start(client); 
+            }
+
+            static void ClientHandler(object o)
+            {
+                /* Communication with the Client */
+                TcpClient client = o as TcpClient;
+                NetworkStream stream = client.GetStream();
+                int i;
+                Byte[] bytes = new Byte[256]; 
+                while((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    Console.WriteLine(stream);
+                }
+            }
         }
     }
 }
