ff0ea8c43176e291ce61f8dc72109e151987c5b8
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2023-01-07 22:02:54 +0100

    SS21 Aufg3 bissle was halt

---
+++ Prüfungen/SS_21/Aufgabe-3/Aufgabe-3/ArduinoDevice.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Aufgabe_3
+{
+    class ArduinoDevice
+    {
+        private double _temperatur = 0.0;
+        private SerialPort _serialPort;
+        private double _grenzwert = 80.0;
+        private Lüfter _lüfter1 = new Lüfter();
+        /* Passendes Delegat und Event */
+        public delegate void delegate_TempAlarm(); 
+        public event delegate_TempAlarm LüfterEinEvent; 
+
+
+        public ArduinoDevice()
+        {
+            _serialPort = new SerialPort();
+            _serialPort.PortName = "COM3";
+            _serialPort.BaudRate = 9600;
+            _serialPort.Open();
+            Thread.Sleep(2000);
+
+            /* Lüfter am Event anmelden */
+            LüfterEinEvent += _lüfter1.LüfterEin;
+
+            Thread arduinoThread = new Thread(LeseTemp);
+            arduinoThread.IsBackground = true;
+            arduinoThread.Start();
+        }
+
+        /* Property zu _temperatur */
+        public double Temperatur
+        {
+            get { return _temperatur; }
+            set
+            {
+                _temperatur = value; 
+                /* Event ausführen */
+                if((LüfterEinEvent != null) && (_temperatur > _grenzwert))
+                {
+                    LüfterEinEvent();
+                }
+            }
+        }
+
+        public void LüfterEin()
+        {
+
+        }
+
+        private void LeseTemp()
+        {
+
+        }
+    }
+}
