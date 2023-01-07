4c9881bf508edaf4d82ee2362b1c7d04cc09dd09
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2023-01-07 23:20:36 +0100

    SS21 Aufg5 done

---
+++ Prüfungen/SS_21/Aufgabe-5/Aufgabe-5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Aufgabe_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add(1);
+            list.Add(3);
+            list.Insert(list.Count - 2, 2); 
+        }
+    }
+}
