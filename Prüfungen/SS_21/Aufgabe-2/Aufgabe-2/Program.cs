8aca1e687640c5bd828b5d8adb60845310c08121
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2023-01-07 21:36:15 +0100

    SS21 Aufg1+2 done

---
+++ Prüfungen/SS_21/Aufgabe-2/Aufgabe-2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading; 
+
+namespace Aufgabe_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* b.) */
+            /* Hintergrund-Thread */
+            Thread neuerThread = new Thread(StatusAusgeben);
+            neuerThread.IsBackground = true;
+            neuerThread.Start();
+
+            /* Zählung aller int-Werte, die die Ziffer "7" enthalten */
+            int count = 0;
+            for (int i = 0; i < Int32.MaxValue; i++)
+            {
+                /* Contains function only works on string so convert to string then check */
+                string s = i.ToString();
+                if (s.Contains("7"))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("int: {0} Werte mit Ziffer 7!", count);
+        }
+        static void StatusAusgeben()
+        {
+            /* Endlosschleife mit Ausgabe */
+            while (true)
+            {
+                Console.WriteLine("Zählung läuft...");
+            }
+        }
+    }
+}
--- Prüfungen/SS_21/Aufgabe-2/Aufgabe-2/Properties/AssemblyInfo.cs
+++ Prüfungen/SS_21/Aufgabe-2/Aufgabe-2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Aufgabe-1")]
+[assembly: AssemblyTitle("Aufgabe-2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Aufgabe-1")]
+[assembly: AssemblyProduct("Aufgabe-2")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-[assembly: Guid("9a118fd1-587b-4c79-be6f-d46046bf9bcb")]
+[assembly: Guid("d8e3f01f-2cb5-43e1-8744-3efcd6357a92")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
 //
