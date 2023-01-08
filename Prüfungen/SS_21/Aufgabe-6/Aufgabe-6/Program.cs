504cb14123ed0b901923c66d1c068b7a0052c545
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2023-01-08 15:24:34 +0100

    SS21 Aufg6 done

--- Prüfungen/SS_21/Aufgabe-6/Aufgabe-6/Program.cs
+++ Prüfungen/SS_21/Aufgabe-6/Aufgabe-6/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Aufgabe_6
 {
@@ -10,6 +12,20 @@ namespace Aufgabe_6
     {
         static void Main(string[] args)
         {
+            /* initialize */
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+
+            /* StreamReader needs the file path */
+            using (StreamReader reader = new StreamReader(openFileDialog.FileName))
+            {
+                string text = reader.ReadToEnd();
+            }
+
+            /* Alternative */
+            //StreamReader reader = new StreamReader(openFileDialog.FileName);
+            //string text = reader.ReadToEnd();
+            //reader.Dispose(); 
 
         }
     }
