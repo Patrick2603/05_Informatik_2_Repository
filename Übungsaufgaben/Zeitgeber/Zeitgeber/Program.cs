98621b66fd4ae4b7d6cdbe97b37b41f0ee4bf64d
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2023-01-11 14:00:59 +0100

    Zeitgeber done

---
+++ Übungsaufgaben/Zeitgeber/Zeitgeber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zeitgeber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void Ausgabe(Zeitgeber z)
+            {
+                Console.WriteLine("tick");
+            }
+
+            void Main()
+            {
+                Zeitgeber z = new Zeitgeber();
+                z.Tick += Ausgabe;
+                z.Start();
+            }
+        }
+    }
+}
---
+++ Übungsaufgaben/Zeitgeber/Zeitgeber/Zeitgeber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading; 
+
+namespace Zeitgeber
+{
+    class Zeitgeber
+    {
+        public delegate void TickDelegate(Zeitgeber quelle);
+        public event TickDelegate Tick;
+
+        public void Start()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+
+                if (Tick != null)
+                {
+                    Tick(this);
+                }
+            }
+        }
+    }
+}
