6986f39e4e43370854c730534aea32f7f015162b
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2023-01-11 11:40:11 +0100

    Echolot done

--- Übungsaufgaben/Echolot/Echolot/Geräusch.cs
+++ Übungsaufgaben/Echolot/Echolot/Geräusch.cs
@@ -3,12 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading; 
 
 namespace Echolot
 {
     class Geräusch
     {
+        Random _random = new Random();
+        
+        public void Ping()
+        {
+            while(true)
+            {
+                int time = _random.Next(250, 1000); 
+                Thread.Sleep(time);
+                Console.Write("Ping ");
+            }
+        }
 
-
+        public void Pong()
+        {
+            while (true)
+            {
+                int time = _random.Next(250, 1000); 
+                Thread.Sleep(time);
+                Console.Write("Pong ");
+            }
+        }
     }
 }
--- Übungsaufgaben/Echolot/Echolot/Program.cs
+++ Übungsaufgaben/Echolot/Echolot/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading; 
 
 namespace Echolot
 {
@@ -10,6 +11,18 @@ namespace Echolot
     {
         static void Main(string[] args)
         {
+            Geräusch g = new Geräusch(); 
+
+            Thread pingThread = new Thread(g.Ping);
+            pingThread.IsBackground = true;
+            pingThread.Start();
+
+            Thread pongThread = new Thread(g.Pong);
+            pongThread.IsBackground = true;
+            pongThread.Start();
+
+            Thread.Sleep(1000);
+            Console.ReadLine(); 
         }
     }
 }
