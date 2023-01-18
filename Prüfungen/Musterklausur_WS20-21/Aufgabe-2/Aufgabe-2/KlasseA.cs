66e292a07cc62c3c34246e1b92ee992efe2b484c
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2023-01-18 23:44:11 +0100

    Musterklausur WS20-21 Aufg 2 created

---
+++ Prüfungen/Musterklausur_WS20-21/Aufgabe-2/Aufgabe-2/KlasseA.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aufgabe_2
+{
+    class KlasseA
+    {
+        private Mutex _m;
+        public KlasseA(Mutex m)
+        {
+            _m = m;
+        }
+        public void Methode1()
+        {
+            _m.WaitOne();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("1");
+                Thread.Sleep(100);
+            }
+            _m.ReleaseMutex();
+        }
+        public void Methode2()
+        {
+            _m.WaitOne();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("2");
+                Thread.Sleep(100);
+            }
+            _m.ReleaseMutex();
+        }
+    }
+}
