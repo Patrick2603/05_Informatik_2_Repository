0d0276205c96641283ee1063fd234e464ae05e83
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2023-01-18 16:35:23 +0100

    WS20-21 Aufg4 done

---
+++ Prüfungen/WS_20-21/Aufgabe-4/Aufgabe-4/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace Aufgabe_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b1 = new System.Windows.Forms.Button();
+            this.n1 = new System.Windows.Forms.NumericUpDown();
+            this.t1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.n1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(91, 12);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(75, 23);
+            this.b1.TabIndex = 0;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // n1
+            // 
+            this.n1.Location = new System.Drawing.Point(12, 12);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(61, 20);
+            this.n1.TabIndex = 1;
+            // 
+            // t1
+            // 
+            this.t1.Location = new System.Drawing.Point(12, 41);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(100, 20);
+            this.t1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.t1);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.b1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.n1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.NumericUpDown n1;
+        private System.Windows.Forms.TextBox t1;
+    }
+}
+
---
+++ Prüfungen/WS_20-21/Aufgabe-4/Aufgabe-4/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aufgabe_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            int value = (int) n1.Value;
+            value *= 2; 
+            t1.Text = value.ToString(); 
+        }
+    }
+}
