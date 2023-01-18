9d3874887dcd64bcd16056efbe6276aeeb6febad
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2023-01-18 17:10:45 +0100

    WS20-21 Aufg5 done

---
+++ Prüfungen/WS_20-21/Aufgabe-5/Aufgabe-5/Artikel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_5
+{
+    class Artikel
+    {
+        private string _bezeichnung;
+        public Artikel(string bezeichnung)
+        {
+            _bezeichnung = bezeichnung;
+        }
+
+        public string Bezeichnung
+        {
+            get
+            {
+                return _bezeichnung;
+            }
+        }
+    }
+}
--- Prüfungen/WS_20-21/Aufgabe-5/Aufgabe-5/Program.cs
+++ Prüfungen/WS_20-21/Aufgabe-5/Aufgabe-5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,19 @@ namespace Aufgabe_5
     {
         static void Main(string[] args)
         {
+            ArrayList l = new ArrayList();
+            l.Add(new Artikel("A"));
+            l.Add(new Artikel("B"));
+            l.Add(new Artikel("C"));
 
+            foreach(object o in l)
+            {
+                /* Downcast */
+                Artikel s = o as Artikel;
+                /* Get value from Property */
+                Console.WriteLine(s.Bezeichnung);
+            }
+            
         }
     }
 }
