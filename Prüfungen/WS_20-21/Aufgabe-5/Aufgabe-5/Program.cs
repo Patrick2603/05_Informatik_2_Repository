22542b8f28d6689333ce67dcac0291a89fb147be
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2023-01-18 16:37:58 +0100

    WS20-21 Aufg5 created

---
+++ Prüfungen/WS_20-21/Aufgabe-5/Aufgabe-5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
