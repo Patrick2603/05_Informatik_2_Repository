7d9f0eb1dfd1e9123eb7a49a6a43f5ac74908545
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2023-01-18 23:41:14 +0100

    Musterklausur WS20-21 Aufg 1 done

--- Prüfungen/Musterklausur_WS20-21/Aufgabe-1/Aufgabe-1/Program.cs
+++ Prüfungen/Musterklausur_WS20-21/Aufgabe-1/Aufgabe-1/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Aufgabe_1
@@ -10,6 +12,23 @@ namespace Aufgabe_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Name des zu öffnenden Ports: ");
+            string port = Console.ReadLine();
+
+            SerialPort serialport = new SerialPort();
+            serialport.PortName = port; 
+            serialport.BaudRate = 19200;
+            serialport.DtrEnable = false;
+
+            serialport.Open();
+            Thread.Sleep(3000); 
+
+            Console.WriteLine("Zu übertragender Text: ");
+            string text = Console.ReadLine();
+
+            /* method exists functionally in solutions */
+            serialPort.Write(string text);
+            serialport.Close(); 
         }
     }
 }
