21fe05a3eb1c35c995365b1bf06e63ffad76356b
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2023-01-18 15:51:29 +0100

    WS20-21 Aufg2 done

---
+++ Prüfungen/WS_20-21/Aufgabe-2/Aufgabe-2/KlasseA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aufgabe_2
+{
+    class KlasseA
+    {
+        private Mutex _m;
+        public KlasseA(Mutex m)
+        {
+            _m = m;
+        }
+
+        /* 5 times A is getting printed at first. Mutex synchronises the two threads. Only one can run at a time */
+        public void Methode()
+        {
+            _m.WaitOne();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("A");
+                Thread.Sleep(1000);
+            }
+            _m.ReleaseMutex();
+        }
+    }
+}
---
+++ Prüfungen/WS_20-21/Aufgabe-2/Aufgabe-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aufgabe_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex x = new Mutex();
+            KlasseA o = new KlasseA(x);
+            /* new Thread is getting started and because of Sleep it is executed first */
+            Thread t = new Thread(o.Methode);
+            t.Start();
+            Thread.Sleep(500);
+
+            /* mutex blocks it so no one else can access it until it is getting released */
+            x.WaitOne();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("B");
+                Thread.Sleep(2000);
+            }
+            x.ReleaseMutex();
+        }
+    }
+}
