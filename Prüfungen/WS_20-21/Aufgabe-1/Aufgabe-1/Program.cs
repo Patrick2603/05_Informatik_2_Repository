929ac7799c12747c0f3e1a3a4dbf1c75e6ec58b9
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2023-01-18 15:40:12 +0100

    WS20-21 Aufg1 done

--- Prüfungen/WS_20-21/Aufgabe-1/Aufgabe-1/Program.cs
+++ Prüfungen/WS_20-21/Aufgabe-1/Aufgabe-1/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
 
 namespace Aufgabe_1
 {
@@ -10,7 +13,37 @@ namespace Aufgabe_1
     {
         static void Main(string[] args)
         {
+            SerialPort serialPort = new SerialPort();
+            serialPort.PortName = "COM5";
+            serialPort.BaudRate = 9600;
+            serialPort.DtrEnable = true;
 
+            try
+            {
+                serialPort.Open();
+                Thread.Sleep(3000);
+
+                byte[] command = new byte[] { 0x00, 0x01 };
+                serialPort.Write(command, 0, command.Length);
+            }
+
+            catch(IOException)
+            {
+                Console.WriteLine("IO Exception");
+            }
+            catch (TimeoutException)
+            {
+                Console.WriteLine("Timeout Exception");
+            }
+            catch
+            {
+                Console.WriteLine("Andere Exception");
+            }
+
+            finally
+            {
+                serialPort.Close(); 
+            }
         }
     }
 }
