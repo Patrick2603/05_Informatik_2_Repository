7c755024efb23aaf7cd012f5d7aa325be32cf014
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2023-01-18 14:53:44 +0100

    SS21 Aufg5 chatGPT war einfach falsch

--- Prüfungen/SS_21/Aufgabe-5/Aufgabe-5/Program.cs
+++ Prüfungen/SS_21/Aufgabe-5/Aufgabe-5/Program.cs
@@ -14,8 +14,9 @@ namespace Aufgabe_5
             ArrayList list = new ArrayList();
             list.Add(1);
             list.Add(3);
-            /* ist das wirkich -2, nicht -2 sogar chatGPT sagt das */
-            list.Insert(list.Count - 1, 2); 
+            /* oke chatGPT hatte doch nicht recht: Es wird zb eine List erstellt { 1, 2, 3, 4 } diese besitzt vier Elemente und jetzt an der Vorletzten stellte etwas hinzuschreiben: 
+             Anzahl 4 - 2 = Index 2 -> vorletzte */ 
+            list.Insert(list.Count - 2, 2); 
         }
     }
 }
