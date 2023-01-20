496f0a0caa6d370ac3ba07c4e6e80bc0a4450532
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2023-01-20 01:03:43 +0100

    so genug für heute, mach ich morgen fertig

---
+++ Prüfungen/Musterklausur_WS20-21/Aufgabe-3/Aufgabe-3/Beobachter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_3
+{
+    class Beobachter
+    {
+        private void Handler(HierPassierts quelle, string name)
+        {
+            switch(name)
+            {
+                case "x":
+                    Console.WriteLine(quelle);
+                    break;
+                case "y":
+                    Console.WriteLine(quelle);
+                    break; 
+            }
+        }
+
+
+    }
+}
---
+++ Prüfungen/Musterklausur_WS20-21/Aufgabe-3/Aufgabe-3/HierPassierts.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_3
+{
+    class HierPassierts
+    {
+        private int _x;
+        private int _y;
+        
+        public delegate void WertGeändertHandler(HierPassierts quelle, string name); 
+        public event WertGeändertHandler WertGeändert; 
+
+        public HierPassierts(int x, int y)
+        {
+            _x = x;
+            _y = y; 
+        }
+
+        public int x
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+                OnWertGeändertChanged(this, x); 
+            }
+        }
+
+        public int y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+                OnWertGeändertChanged(this, y);
+            }
+        }
+
+        private void OnWertGeändertChanged(string s)
+        {
+            if(WertGeändert != null)
+            {
+                WertGeändert(this, s);
+            }
+        }
+
+    }
+}
